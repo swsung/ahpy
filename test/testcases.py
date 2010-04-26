69ce6a6a1d4f8bb4612deef50d1365ba6026277f
petrias <petrias@ee722efe-0227-0410-b297-1b65bf2f2943>
2010-04-26 13:37:55 +0000

    make test runnable using doctest

--- test/testcases.py
+++ test/testcases.py
@@ -1,37 +1,34 @@
-from decimal import Decimal
-from ahpy.core import calculateWeights, calculateConsistency
+"""
+Saaty: 'How to make a decision', p. 27
+According to Saaty, the last value rounds to 0.11. 
+Todo: More tests to find out whether there's something
+needing fixing or improving.
 
-# Saaty: 'How to make a decision', p. 27
-# SAccording to Saaty, the last value rounds to 0.11. 
-# Todo: More tests to find out whether there's something
-# needing fixing or improving.
+>>> from decimal import Decimal
+>>> from ahpy.core import calculateWeights, calculateConsistency
+>>> arr1 = [[1,3,5], [1./3., 1,3], [1./5., 1./3., 1]] 
+>>> print  calculateWeights(arr1, rounding=4)
+[Decimal('0.6370'), Decimal('0.2583'), Decimal('0.1047')]
+>>> print calculateConsistency(arr1)
+"""
 
-arr1 = [[1,3,5], [1./3., 1,3], [1./5., 1./3., 1]] 
-w1 = calculateWeights(arr1, rounding=4)
-try:
-   assert(w1==[Decimal('0.6370'), Decimal('0.2583'), Decimal('0.1047')])
-except:
-   print w1
-   
-print calculateConsistency(arr1)
-# 0.037
+"""
+Saaty: How to make a decision', p. 34'",
 
-# Saaty: How to make a decision', p. 34'",
-
-arr2 = [
+>>> arr2 = [
 [1    , 2    , 3    , 4    , 5],
 [1./2., 1    , 2    , 3    , 4],
 [1./3., 1./2., 1    , 2    , 3],
 [1./4., 1./3., 1./2., 1    , 2],
 [1./5., 1./4., 1./3., 1./2., 1]]
+>>> print calculateWeights(arr2,rounding=3)
+[Decimal('0.419'), Decimal('0.263'), Decimal('0.160'), Decimal('0.097'), Decimal('0.062')]
+>>> print calculateConsistency(arr2)
 
-w2 = calculateWeights(arr2,rounding=3)
+"""
 
-try:
-   assert(w2 == [Decimal('0.419'), Decimal('0.263'), Decimal('0.160'), Decimal('0.097'), Decimal('0.062')])
-except AssertionError:
-   print w2
 
-print calculateConsistency(arr2)
-   
-# 0.015
+if __name__ == "__main__":
+   import doctest
+   doctest.testmod()
+
